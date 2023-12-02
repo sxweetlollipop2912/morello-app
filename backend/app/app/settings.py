6dc4d5c497a4b1cfc5565f65c05b62f20af88de1
sangpham2710 <tansang.27.10.2003@gmail.com>
2023-12-02 16:05:54 +0700

    add user model

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-qbw_fum*sfdfpq*xl4fedyh*t53shoba2o3$k!2mjjx@&w)jr4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -126,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
--- backend/app/core/models.py
+++ backend/app/core/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
+
+    def _create_user(self, email, password, **kwargs):
+        if not email:
+            raise ValueError('User must have an email address.')
+
+        email = self.normalize_email(email)
+        is_staff = kwargs.pop('is_staff', False)
+        is_superuser = kwargs.pop('is_superuser', False)
+
+        user = self.model(
+            email=email,
+            is_active=True,
+            is_staff=is_staff,
+            is_superuser=is_superuser,
+            **kwargs
+        )
+        user.set_password(password)
+
+        user.save(using=self._db)
+
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        return self._create_user(email, password, is_staff=True, is_superuser=True, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
