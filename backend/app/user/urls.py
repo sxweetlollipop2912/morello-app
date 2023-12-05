025a4a8091d3496f36191deba13b15673bc22662
sangpham2710 <tansang.27.10.2003@gmail.com>
2023-12-06 00:45:21 +0700

    add create user with /api/user/create/

---
+++ backend/app/user/urls.py
@@ -0,0 +1,12 @@
+"""
+URL mappings for the user API.
+"""
+from django.urls import path
+
+from user import views
+
+app_name = 'user'
+
+urlpatterns = [
+    path('register/', views.CreateUserView.as_view(), name='create'),
+]
