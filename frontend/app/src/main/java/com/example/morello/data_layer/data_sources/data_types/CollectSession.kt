e0fd4c0a231035568b017eabfe05d1045841edb0
ltp1405 <lamtungphan1405@gmail.com>
2023-12-07 22:21:30 +0700

    update id field of CollectSession

--- frontend/app/src/main/java/com/example/morello/data_layer/data_sources/data_types/CollectSession.kt
+++ frontend/app/src/main/java/com/example/morello/data_layer/data_sources/data_types/CollectSession.kt
@@ -3,7 +3,7 @@ package com.example.morello.data_layer.data_sources.data_types
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class CollectSession(
-    @JsonProperty("id") val id: String,
+    @JsonProperty("id") val id: Int,
     @JsonProperty("start") val start: String,
     @JsonProperty("due") val due: String,
     @JsonProperty("description") val description: String,
