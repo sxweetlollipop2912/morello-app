58aff7a966dd8ef605046e913e77af2fdede7e8e
ltp1405 <lamtungphan1405@gmail.com>
2023-12-07 22:20:11 +0700

    remove redundant file

--- frontend/app/src/main/java/com/example/morello/data_layer/repositories/UserRepository.kt
+++ frontend/app/src/main/java/com/example/morello/data_layer/repositories/UserRepository.kt
@@ -1,11 +1,37 @@
 package com.example.morello.data_layer.repositories
 
+import com.example.morello.data_layer.data_sources.RemoteUserDataSource
+import com.example.morello.data_layer.data_sources.SettingDataStore
 import com.example.morello.data_layer.data_sources.data_types.User
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
-class UserRepository {
-    fun getUserById(id: Int): Flow<User> = TODO()
-    suspend fun updateUserDetail(user: User): Nothing = TODO()
+class UserRepository(
+    private val remoteUserDataSource: RemoteUserDataSource,
+    private val settingDataStore: SettingDataStore
+) {
+    val isLoggedIn: Flow<Boolean> = settingDataStore.getToken().map {
+        it.isNotEmpty()
+    }
+
+    suspend fun login(username: String, password: String) {
+        val token = remoteUserDataSource.login(username, password)
+        settingDataStore.setToken(token)
+    }
+
+    suspend fun register(username: String, password: String, email: String) {
+        val token = remoteUserDataSource.register(username, password, email)
+        settingDataStore.setToken(token)
+    }
+
+    suspend fun logout() {
+        settingDataStore.setToken("")
+    }
+
+    suspend fun updateUserDetail(user: User) {
+        TODO()
+    }
+    
     fun getLeadedGroups(userId: Int): Flow<List<Int>> = TODO()
     fun getModeratedGroups(userId: Int): Flow<List<Int>> = TODO()
 }
