943cf80cebe8dde419e5cfac8ae82e3f70e864bd
ltp1405 <lamtungphan1405@gmail.com>
2023-12-07 22:14:01 +0700

    update User Api

---
+++ frontend/app/src/main/java/com/example/morello/data_layer/data_sources/apis/client/UserApi.kt
@@ -0,0 +1,18 @@
+package com.example.morello.data_layer.data_sources.apis.client
+
+import com.example.morello.data_layer.data_sources.data_types.LoginRequest
+import com.example.morello.data_layer.data_sources.data_types.RegisterRequest
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+
+interface UserApi {
+    @GET("users/login")
+    suspend fun login(@Body loginRequest: LoginRequest): Call<String>
+
+    @GET("users/logout")
+    suspend fun logout(): Call<Nothing>
+
+    @GET("users/register")
+    suspend fun register(@Body registerRequest: RegisterRequest): Call<String>
+}
