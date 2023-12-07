8c50044f42433f06812678050a31cd9d2130c107
ltp1405 <lamtungphan1405@gmail.com>
2023-12-07 22:20:33 +0700

    update UserApi

--- frontend/app/src/main/java/com/example/morello/data_layer/data_sources/apis/client/UserApi.kt
+++ frontend/app/src/main/java/com/example/morello/data_layer/data_sources/apis/client/UserApi.kt
@@ -2,6 +2,7 @@ package com.example.morello.data_layer.data_sources.apis.client
 
 import com.example.morello.data_layer.data_sources.data_types.LoginRequest
 import com.example.morello.data_layer.data_sources.data_types.RegisterRequest
+import com.example.morello.data_layer.data_sources.data_types.User
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -15,4 +16,7 @@ interface UserApi {
 
     @GET("users/register")
     suspend fun register(@Body registerRequest: RegisterRequest): Call<String>
+
+    @GET("users/{userId}")
+    abstract fun fetchUserDetail(userId: Int): Call<User>
 }
