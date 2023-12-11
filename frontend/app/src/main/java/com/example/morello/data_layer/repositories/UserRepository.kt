474ed5aaca4282f3c4d0f97520a80852aadfeaf1
ltp1405 <lamtungphan1405@gmail.com>
2023-12-11 23:51:53 +0700

    update UserRepo for login

--- frontend/app/src/main/java/com/example/morello/data_layer/repositories/UserRepository.kt
+++ frontend/app/src/main/java/com/example/morello/data_layer/repositories/UserRepository.kt
@@ -1,16 +1,17 @@
 package com.example.morello.data_layer.repositories
 
+import android.util.Log
 import com.example.morello.data_layer.data_sources.RemoteUserDataSource
-import com.example.morello.data_layer.data_sources.SettingDataStore
+import com.example.morello.data_layer.data_sources.SettingDataSource
 import com.example.morello.data_layer.data_sources.data_types.User
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 class UserRepository(
     private val remoteUserDataSource: RemoteUserDataSource,
-    private val settingDataStore: SettingDataStore
+    private val settingDataSource: SettingDataSource
 ) {
-    val isLoggedIn: Flow<Boolean> = settingDataStore.getToken().map {
+    val isLoggedIn: Flow<Boolean> = settingDataSource.getToken().map {
         it.isNotEmpty()
     }
 
@@ -19,17 +20,22 @@ class UserRepository(
     }
 
     suspend fun login(username: String, password: String) {
-        val token = remoteUserDataSource.login(username, password)
-        settingDataStore.setToken(token)
+        try {
+            val token = remoteUserDataSource.login(username, password)
+            Log.d("UserRepository", "Token: $token")
+            settingDataSource.setToken(token)
+        } catch (e: Exception) {
+            Log.d("UserRepository", e.toString())
+        }
     }
 
     suspend fun register(username: String, password: String, email: String) {
         val token = remoteUserDataSource.register(username, password, email)
-        settingDataStore.setToken(token)
+        settingDataSource.setToken(token)
     }
 
     suspend fun logout() {
-        settingDataStore.setToken("")
+        settingDataSource.setToken("")
     }
 
     suspend fun updateUserDetail(user: User) {
