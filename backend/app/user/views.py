3c23306f27f619a83b20f8daa64fb5297f0f3aad
sangpham2710 <tansang.27.10.2003@gmail.com>
2023-12-13 22:18:20 +0700

    fix stuff

--- backend/app/user/views.py
+++ backend/app/user/views.py
@@ -9,16 +9,14 @@ from core.models import (
 from user.serializers import (
     UserSerializer,
 )
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# GET /users/: List all users. This corresponds to the list() method in the viewset.
-# POST /users/: Create a new user. This corresponds to the create() method in the viewset.
-# GET /users/{id}/: Retrieve a specific user by their ID. This corresponds to the retrieve() method in the viewset.
-# PUT /users/{id}/: Update a specific user by their ID. This corresponds to the update() method in the viewset.
-# PATCH /users/{id}/: Partially update a specific user by their ID. This corresponds to the partial_update() method in the viewset.
-# DELETE /users/{id}/: Delete a specific user by their ID. This corresponds to the destroy() method in the viewset.
+# GET /users/: List all users. This corresponds to the list() method in the viewset. # noqa
+# POST /users/: Create a new user. This corresponds to the create() method in the viewset. # noqa
+# GET /users/{id}/: Retrieve a specific user by their ID. This corresponds to the retrieve() method in the viewset. # noqa
+# PUT /users/{id}/: Update a specific user by their ID. This corresponds to the update() method in the viewset. # noqa
+# PATCH /users/{id}/: Partially update a specific user by their ID. This corresponds to the partial_update() method in the viewset. # noqa
+# DELETE /users/{id}/: Delete a specific user by their ID. This corresponds to the destroy() method in the viewset. # noqa
 # See: https://www.django-rest-framework.org/api-guide/viewsets/#modelviewset
 
 
