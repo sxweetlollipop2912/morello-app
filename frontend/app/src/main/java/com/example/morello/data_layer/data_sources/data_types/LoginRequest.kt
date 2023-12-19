9b51c1e8641add4fd4845ecda7a532470dbd74c9
ltp1405 <lamtungphan1405@gmail.com>
2023-12-19 23:13:33 +0700

    update type field

--- frontend/app/src/main/java/com/example/morello/data_layer/data_sources/data_types/LoginRequest.kt
+++ frontend/app/src/main/java/com/example/morello/data_layer/data_sources/data_types/LoginRequest.kt
@@ -3,6 +3,6 @@ package com.example.morello.data_layer.data_sources.data_types
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class LoginRequest(
-    @JsonProperty("username") val username: String,
+    @JsonProperty("email") val email: String,
     @JsonProperty("password") val password: String,
 )
