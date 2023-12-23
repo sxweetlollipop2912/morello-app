15c1e474089216a2aee5a870a548b2fe431d43c2
sangpham2710 <tansang.27.10.2003@gmail.com>
2023-12-23 18:13:20 +0700

    add user endpoints

--- backend/app/user/urls.py
+++ backend/app/user/urls.py
@@ -2,18 +2,24 @@
 URL mappings for the user API.
 """
 from django.urls import path
-from rest_framework.routers import SimpleRouter
 from .views import UserViewSet
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
-app_name = "user"
-router = SimpleRouter()
-router.register(r"users", UserViewSet, basename="user")
-
 urlpatterns = [
+    path(
+        "users/me/",
+        UserViewSet.as_view(
+            {
+                "get": "list",
+                "put": "update",
+                "patch": "partial_update",
+                "delete": "destroy",
+            }
+        ),
+    ),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-] + router.urls
+]
--- backend/app/user/views.py
+++ backend/app/user/views.py
@@ -1,12 +1,16 @@
 """
 Views for the user API.
 """
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from user.models import User
 
 from user.serializers import (
     UserSerializer,
 )
+
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from django.http import Http404
 
 # GET /users/: List all users. This corresponds to the list() method in the viewset. # noqa
 # POST /users/: Create a new user. This corresponds to the create() method in the viewset. # noqa
@@ -18,5 +22,40 @@ from user.serializers import (
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all().order_by("id")
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return User.objects.filter(id=self.request.user.id)
+
+    def get_object(self):
+        obj = get_object_or_404(User, id=self.request.user.id)
+        self.check_object_permissions(self.request, obj)
+        return obj
+
+    def check_instance_id(self, instance):
+        if instance.id != self.request.user.id:
+            raise Http404
+
+    def list(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        raise Http404
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.check_instance_id(instance)
+        return super().update(request, *args, **kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.check_instance_id(instance)
+        return super().partial_update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.check_instance_id(instance)
+        return super().destroy(request, *args, **kwargs)
