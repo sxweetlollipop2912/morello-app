f7ef427912c727dd45c1f9dce82c325505d74738
Hoai Nam <hoainamnguyennhu7@gmail.com>
2023-12-23 22:30:20 +0700

    update balance entry model

--- backend/app/balance/models.py
+++ backend/app/balance/models.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 from django.db import models
-from group.models import Group
 from collect_session.models import CollectSession
+from group.models import Group
 
 # Create your models here.
 
@@ -9,6 +10,14 @@ class BalanceEntry(models.Model):
     group_id = models.ForeignKey(
         Group, on_delete=models.CASCADE, related_name="balance_entries"
     )
+    name = models.CharField(max_length=255)
     amount = models.IntegerField()
     description = models.CharField(max_length=255)
-    date = models.DateTimeField()
+    date = models.DateTimeField(default=str(datetime.now()))
+    session_id = models.ForeignKey(
+        CollectSession,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="balance_entries",
+    )
