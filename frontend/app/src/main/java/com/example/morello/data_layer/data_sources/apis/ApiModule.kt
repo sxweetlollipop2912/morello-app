84e146a58bca473414cfb4b11c9b56a551fb0996
ltp1405 <lamtungphan1405@gmail.com>
2024-01-02 13:50:59 +0700

    update binding

--- frontend/app/src/main/java/com/example/morello/data_layer/data_sources/apis/ApiModule.kt
+++ frontend/app/src/main/java/com/example/morello/data_layer/data_sources/apis/ApiModule.kt
@@ -1,6 +1,7 @@
 package com.example.morello.data_layer.data_sources.apis
 
 import com.example.morello.data_layer.data_sources.apis.client.AuthRetrofitClient
+import com.example.morello.data_layer.data_sources.apis.mocked_apis.MockedGroupApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -24,10 +25,20 @@ class ApiModule {
     @Provides
     @ViewModelScoped
     fun bindGroupApi(@AuthRetrofitClient retrofit: Retrofit): GroupApi =
-        retrofit.create(GroupApi::class.java)
+        MockedGroupApi()
 
     @Provides
     @ViewModelScoped
     fun bindMemberApi(@AuthRetrofitClient retrofit: Retrofit): MemberApi =
         retrofit.create(MemberApi::class.java)
+
+    @Provides
+    @ViewModelScoped
+    fun bindCollectSessionApi(@AuthRetrofitClient retrofit: Retrofit): CollectSessionApi =
+        retrofit.create(CollectSessionApi::class.java)
+
+    @Provides
+    @ViewModelScoped
+    fun bindModeratorApi(@AuthRetrofitClient retrofit: Retrofit): ModeratorApi =
+        retrofit.create(ModeratorApi::class.java)
 }
