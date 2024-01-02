b6421733f1e892f148277053fbd0b573a896316a
ltp1405 <lamtungphan1405@gmail.com>
2024-01-02 13:52:46 +0700

    update collect session repo

--- frontend/app/src/main/java/com/example/morello/data_layer/repositories/CollectSessionRepository.kt
+++ frontend/app/src/main/java/com/example/morello/data_layer/repositories/CollectSessionRepository.kt
@@ -1,17 +1,27 @@
 package com.example.morello.data_layer.repositories
 
+import com.example.morello.data_layer.data_sources.RemoteCollectSessionDataSource
 import com.example.morello.data_layer.data_sources.data_types.collect_sessions.CollectSession
 import com.example.morello.data_layer.data_sources.data_types.collect_sessions.CollectSessionEntry
+import com.example.morello.data_layer.data_sources.data_types.collect_sessions.NewCollectSession
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class CollectSessionRepository @Inject constructor() {
-    fun getCollectSessionEntries(collectSessionId: Int): Flow<List<CollectSessionEntry>> = TODO()
+class CollectSessionRepository @Inject constructor(
+    private val remoteCollectSessionDataSource: RemoteCollectSessionDataSource
+) {
+    suspend fun getCollectSessionEntries(collectSessionId: Int): List<CollectSessionEntry> = TODO()
     suspend fun updateCollectSessionEntry(collectSessionEntry: CollectSessionEntry): Nothing =
         TODO()
 
     fun getCollectSessionById(id: Int): Flow<CollectSession> = TODO()
     suspend fun deleteCollectSession(collectSession: CollectSession): Nothing = TODO()
     suspend fun updateCollectSession(collectSession: CollectSession): Nothing = TODO()
-    suspend fun createCollectSession(collectSession: CollectSession): Nothing = TODO()
+    suspend fun createCollectSession(groupId: Int, collectSession: NewCollectSession) {
+        if (collectSession.name.isBlank()) {
+            throw Exception("Collect session name cannot be blank")
+        } else if (collectSession.name == "error") {
+            throw Exception("Error creating collect session")
+        }
+    }
 }
