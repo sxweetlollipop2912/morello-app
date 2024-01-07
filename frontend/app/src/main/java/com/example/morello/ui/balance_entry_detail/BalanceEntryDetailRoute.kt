6a6759dc4cbd5945fb636942ada680763b678a17
ltp1405 <lamtungphan1405@gmail.com>
2024-01-07 20:52:28 +0700

    update with view model

--- frontend/app/src/main/java/com/example/morello/ui/balance_entry_detail/BalanceEntryDetailRoute.kt
+++ frontend/app/src/main/java/com/example/morello/ui/balance_entry_detail/BalanceEntryDetailRoute.kt
@@ -1,2 +1,29 @@
 package com.example.morello.ui.balance_entry_detail
 
+import android.util.Log
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+
+@Composable
+fun BalanceEntryDetailRoute(
+    groupId: Int,
+    entryId: Int,
+    viewModel: BalanceEntryDetailViewModel,
+    onBack: () -> Unit,
+) {
+    val uiState = viewModel.uiState
+    LaunchedEffect(key1 = uiState.state) {
+        when (uiState.state) {
+            State.Uninitialized -> {
+                Log.d("BalanceEntryDetailRoute", "BalanceEntryDetailRoute: $groupId, $entryId")
+                viewModel.init(groupId, entryId)
+            }
+
+            else -> {}
+        }
+    }
+    BalanceEntryDetailScreen(
+        uiState = uiState,
+        onBack = onBack,
+    )
+}
