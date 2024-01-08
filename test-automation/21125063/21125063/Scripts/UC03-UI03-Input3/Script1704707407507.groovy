31a5ed5decf05b7dfb94d586f5e93b8716c7fa30
sangpham2710 <tansang.27.10.2003@gmail.com>
2024-01-08 17:24:11 +0700

    finish automation test

---
+++ test-automation/21125063/21125063/Scripts/UC03-UI03-Input3/Script1704707407507.groovy
@@ -0,0 +1,56 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startExistingApplication('com.example.morello')
+
+Mobile.takeScreenshot('C:\\Users\\tansa\\AppData\\Local\\Temp\\screenshot11681619649043454598.png')
+
+Mobile.sendKeys(findTestObject('Object Repository/UC03-UI03/android.widget.EditText'), 'admin@morello.app')
+
+Mobile.sendKeys(findTestObject('Object Repository/UC03-UI03/android.widget.EditText (1)'), 'admin')
+
+Mobile.tap(findTestObject('Object Repository/UC03-UI03/android.widget.Button'), 0)
+
+Mobile.takeScreenshot('C:\\Users\\tansa\\AppData\\Local\\Temp\\screenshot9247040380917115946.png')
+
+Mobile.tap(findTestObject('Object Repository/UC03-UI03/android.view.View'), 0)
+
+Mobile.takeScreenshot('C:\\Users\\tansa\\AppData\\Local\\Temp\\screenshot12377891300602375905.png')
+
+Mobile.tap(findTestObject('Object Repository/UC03-UI03/android.widget.Button (1)'), 0)
+
+Mobile.tap(findTestObject('Object Repository/UC03-UI03/android.widget.Button (1)'), 0)
+
+Mobile.sendKeys(findTestObject('Object Repository/UC03-UI03/android.widget.EditText'), '654321')
+
+Mobile.sendKeys(findTestObject('Object Repository/UC03-UI03/android.widget.EditText (1)'), 'Allo')
+
+Mobile.tap(findTestObject('Object Repository/UC03-UI03/android.widget.Button'), 0)
+
+Mobile.tap(findTestObject('Object Repository/UC03-UI03/android.widget.Button (2)'), 0)
+
+Mobile.tap(findTestObject('Object Repository/UC03-UI03/android.widget.Button (3)'), 0)
+
+Mobile.tap(findTestObject('Object Repository/UC03-UI03/android.widget.TextView - Create'), 0)
+
+Mobile.verifyElementExist(findTestObject('Object Repository/UC03-UI03/android.widget.TextView - Error creating balance entry'), 
+    0)
+
+Mobile.closeApplication()
+
---
+++ test-automation/21125063/21125063/Test Suites/21125063.ts
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>21125063</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>f3c3f81c-6569-469d-b87e-700a31685e12</testSuiteGuid>
+   <testCaseLink>
+      <guid>8f755ccf-7e7e-4e68-bc91-65db8b617327</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UC03-UI01-Input1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e80ee9f5-8b7c-4d41-bf11-d9345476db71</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UC03-UI01-Input2</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c619ba0d-a239-402f-bb2e-05426a470348</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UC03-UI01-Input3</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8bc9ab1a-e432-4db2-9c31-49f6b81610e1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UC03-UI01-Input4</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7990a7b8-871c-4c7f-b142-f7227b11ad47</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UC03-UI01-Input5</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>00534193-ca67-4335-a5d2-21187b3d8efe</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UC03-UI03-Input1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3d5c85d9-d1b2-4782-a674-f971d2d3dce4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UC03-UI03-Input2</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>64996252-8ffb-429d-b834-9abe2879e5be</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UC03-UI03-Input3</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c8ebbaa9-da31-4dd9-a5b7-94ac266af0fa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UC03-UI03-Input4</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b5f3d811-d8de-47d7-9abb-184e8642c87d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UC03-UI03-Input5</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
